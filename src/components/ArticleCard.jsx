90956d992dc1564ae04afd6c967631e209e1fc27
Emily Lipscomb <emilylipscomb4@gmail.com>
2024-03-11 14:47:27 +0000

    App & Article Card updated for SingleArticle functionality

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,18 +1,20 @@
+import { Link } from "react-router-dom";
 import ModeCommentIcon from "@mui/icons-material/ModeComment";
 import ".././stylesheets/ArticleCard.css";
 
-
 function ArticleCard({ article }) {
   return (
-    <div id="article-card">
-      <h1>{article.title}</h1>
-      <h2>By {article.author}</h2>
-      <img src={article.article_img_url} alt="" />
-      <p>
-        <ModeCommentIcon />
-        {article.comment_count}
-      </p>
-    </div>
+    <Link className="article-card-link" to={`/articles/${article.article_id}`}>
+      <div id="article-card">
+        <h1>{article.title}</h1>
+        <h2>By {article.author}</h2>
+        <img src={article.article_img_url} alt={article.title} />
+        <p>
+          <ModeCommentIcon />
+          {article.comment_count}
+        </p>
+      </div>
+    </Link>
   );
 }
 
