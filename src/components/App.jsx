9b4ec4dc97d92b50aea3b0926962820ee7ef8c15
Emily Lipscomb <emilylipscomb4@gmail.com>
2024-03-12 12:12:41 +0000

    App, CommentCard and Comments component updated for PostComment functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,12 +4,18 @@ import Header from "./Header";
 import Home from "./Home";
 import Articles from "./Articles";
 import SingleArticle from "./SingleArticle";
+import UserContext from "../contexts/User";
 
 function App() {
   const [articles, setArticles] = useState([]);
+  const [loggedInUser, setLoggedInUser] = useState({
+    username: "happyamy2016",
+    name: "Amy Happy",
+    avatar_url: "https://vignette1.wikia.nocookie.net/mrmen/images/7/7f/Mr_Happy.jpg/revision/latest?cb=20140102171729",
+  })
 
   return (
-    <>
+    <UserContext.Provider value={{loggedInUser, setLoggedInUser}}>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -23,7 +29,7 @@ function App() {
         />
         <Route path="/articles/:article_id" element={<SingleArticle />} />
       </Routes>
-    </>
+    </UserContext.Provider >
   );
 }
 
--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -4,7 +4,10 @@ function CommentCard({ comment }) {
   return (
     <div className="comment-card">
       <p id="comment-author">{comment.author}</p>
-      <p id="comment-date">Posted {comment.created_at.slice(0, 10)}</p>
+      <p id="comment-date">
+        Posted{" "}
+        {comment.created_at ? comment.created_at.slice(0, 10) : "just now"}
+      </p>
       <p id="comment-body">{comment.body}</p>
       <p>Votes: {comment.votes}</p>
     </div>
