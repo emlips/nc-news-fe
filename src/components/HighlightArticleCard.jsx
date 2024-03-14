96429d948a74720bf97ed9e5854a8ca01b745422
Emily Lipscomb <emilylipscomb4@gmail.com>
2024-03-14 09:58:01 +0000

    nested links in HighlightArticleCard fixed

--- src/components/HighlightArticleCard.jsx
+++ src/components/HighlightArticleCard.jsx
@@ -5,29 +5,41 @@ import ".././stylesheets/HighlightArticleCard.css";
 
 function ArticleCard({ article, setCurrTopic }) {
   return (
-    <Link className="article-card-link" to={`/articles/${article.article_id}`}>
+    <>
       <div id="highlight-article-card">
-        <div id="image">
-          <img src={article.article_img_url} alt={article.title} />
-        </div>
+        <Link
+          className="article-card-link"
+          to={`/articles/${article.article_id}`}
+        >
+          <div id="image">
+            <img src={article.article_img_url} alt={article.title} />
+          </div>
+        </Link>
         <div id="info">
           <h1>{article.title}</h1>
-          <Link className="highlight-topic-link" to={`/articles?topic=${article.topic}`} key={article.topic} onClick={() => {
-              setCurrTopic(article.topic)
-            }} ><h2>{article.topic[0].toUpperCase() + article.topic.slice(1)}</h2></Link>
-            <div className="icons">
-          <p>
-            <ThumbUpOutlinedIcon />
-            {article.votes}
-          </p>
-          <p>
-            <ModeCommentOutlinedIcon />
-            {article.comment_count}
-          </p>
+          <Link
+            className="highlight-topic-link"
+            to={`/articles?topic=${article.topic}`}
+            key={article.topic}
+            onClick={() => {
+              setCurrTopic(article.topic);
+            }}
+          >
+            <h2>{article.topic[0].toUpperCase() + article.topic.slice(1)}</h2>
+          </Link>
+          <div className="icons">
+            <p>
+              <ThumbUpOutlinedIcon />
+              {article.votes}
+            </p>
+            <p>
+              <ModeCommentOutlinedIcon />
+              {article.comment_count}
+            </p>
           </div>
         </div>
       </div>
-    </Link>
+    </>
   );
 }
 
