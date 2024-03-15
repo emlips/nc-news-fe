1ff529aef0b197c43b72a6005e4f3d8b31cc2d21
Emily Lipscomb <emilylipscomb4@gmail.com>
2024-03-15 09:35:22 +0000

    icons added to Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,9 @@
 import { Link } from "react-router-dom";
 import { useContext } from "react";
 import UserContext from "../contexts/User";
+import HomeIcon from '@mui/icons-material/Home';
+import NewspaperIcon from '@mui/icons-material/Newspaper';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
 import "../stylesheets/Header.css";
 
 function Header({ setCurrTopic }) {
@@ -8,18 +11,20 @@ function Header({ setCurrTopic }) {
   return (
     <nav className="header-nav">
       <Link className="header-link" to="/">
-        HOME
+        <HomeIcon /> <p>HOME</p>
       </Link>
       <Link
         className="header-link"
         to="/articles"
         onClick={() => setCurrTopic(undefined)}
       >
-        ARTICLES
+        <NewspaperIcon />
+        <p>ARTICLES</p>
       </Link>
       {loggedInUser ? (
         <Link className="header-link" to={`/profile/${loggedInUser.username}`}>
-          PROFILE
+          <AccountBoxIcon />
+          <p>PROFILE</p>
         </Link>
       ) : (
         <Link className="header-link" to="/login">
